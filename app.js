075f05d3e1db063a1615e72fa30ba4dd476ab629
CodingMileage <[200~72987158+CodingMileage@users.noreply.github.com~>
2023-07-28 23:39:40 -0500

    Added function round that adds logic to determine winner for game

--- app.js
+++ app.js
@@ -1,8 +1,44 @@
 function getComputerChoice() {
-    const cpu = ['Rock', 'Paper', 'Scissors']
+    const cpu = ['rock', 'paper', 'scissors']
 
     const random = Math.floor(Math.random() * cpu.length);
     console.log(random, cpu[random]);
+
+    return cpu[random];
 }
 
-console.log(getComputerChoice())
+// console.log(getComputerChoice())
+
+function round(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return 'Its a tie!';
+    }
+
+    if (playerSelection === 'rock') {
+        if (computerSelection === 'paper') {
+            return 'You lose!';
+        } else if (computerSelection === 'scissors') {
+            return 'You win!';
+        }
+    }
+
+    if (playerSelection === 'paper'){
+        if (computerSelection === 'rock') {
+            return 'You win!';
+        } else if (computerSelection === 'scissors') {
+            return 'You lose!';
+        }
+    }
+
+    if (playerSelection === 'scissors') {
+        if (computerSelection === 'paper') {
+            return 'You win!';
+        } else if (computerSelection === 'rock') {
+            return 'You lose!';
+        }
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(round(playerSelection, computerSelection));

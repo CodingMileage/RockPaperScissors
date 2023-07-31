45f9029e95f5dbc853fcaf989f9de2d18590f070
CodingMileage <[200~72987158+CodingMileage@users.noreply.github.com~>
2023-07-31 00:39:06 -0500

    Removed function to play five rounds. Created buttons for input. Added variable to count player points and cpu points Once points reach 5, game resets. Created a css file to make game more appealing.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
+const buttons = document.querySelectorAll('.btn');
+
+let playerScore = 0;
+let compScore = 0;
+
+let computerSelection = getComputerChoice();
+
+const player = document.querySelector('.player');
+player.textContent = `Player Score: ${playerScore}`;
+
+const computer = document.querySelector('.computer');
+computer.textContent = `Computer Score: ${compScore}`;
+
+const output = document.querySelector('.output');
+output.textContent = "We need you to win!";
+
+
 function getComputerChoice() {
     const cpu = ['rock', 'paper', 'scissors']
 
@@ -13,39 +30,75 @@ function round(playerSelection, computerSelection) {
 
     if (playerSelection === 'rock') {
         if (computerSelection === 'paper') {
+            compScore++;
             return 'You lose!\nCPU chose ' + computerSelection;
         } else if (computerSelection === 'scissors') {
+            playerScore++;
             return 'You win!\nCPU chose ' + computerSelection;
         }
     }
 
     if (playerSelection === 'paper'){
         if (computerSelection === 'rock') {
+            playerScore++;
             return 'You win!\nCPU chose ' + computerSelection;
         } else if (computerSelection === 'scissors') {
+            compScore++;
             return 'You lose!\nCPU chose ' + computerSelection;
         }
     }
 
     if (playerSelection === 'scissors') {
         if (computerSelection === 'paper') {
+            playerScore++;
             return 'You win!\nCPU chose ' + computerSelection;
         } else if (computerSelection === 'rock') {
+            compScore++;
             return 'You lose!\nCPU chose ' + computerSelection;
         }
     }
 }
 
 
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', () => {
+    computerSelection = getComputerChoice();
+    playerSelection = 'rock';
+    game();
+});
+
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', () => {
+    computerSelection = getComputerChoice();
+    playerSelection = 'paper';
+    game();
+});
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', () => {
+    computerSelection = getComputerChoice();
+    playerSelection = 'scissors';
+    game();
+});
+
 function game() {
-    for (i = 0; i < 5; i++) {
-        computerSelection = getComputerChoice();
-        console.log(round(playerSelection, computerSelection))
+
+    output.textContent = round(playerSelection, computerSelection);
+    player.textContent = `Player Score: ${playerScore}`;
+    computer.textContent = `Computer Score: ${compScore}`;
+
+    if (playerScore == 5) {
+        playerScore = 0;
+        compScore = 0;
+        output.textContent = 'You win!';
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
+
+    } else if (compScore == 5) {
+        playerScore = 0;
+        compScore = 0;
+        output.textContent = 'You lost!';
+        player.textContent = `Player Score: ${playerScore}`;
+        computer.textContent = `Computer Score: ${compScore}`;
     }
 }
-
-
-let user = prompt("Rock, Paper or Scissors: ");
-let playerSelection = user.toLowerCase();
-let computerSelection = getComputerChoice();
-game();
